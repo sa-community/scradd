caeb299bc31cf1bd208cb47c5dd70b1f8b838c3b
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-08-06 10:20:17 -0500

    use cv2
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- modules/embeds/config.ts
+++ modules/embeds/config.ts
@@ -1,6 +1,6 @@
 import type { ChatInputCommandInteraction } from "discord.js";
 
-import { channelMention } from "discord.js";
+import { channelMention, ComponentType, MessageFlags } from "discord.js";
 import mongoose from "mongoose";
 
 import constants from "../../common/constants.ts";
@@ -43,17 +43,29 @@ export default async function configEmbeds(
 	await config.save();
 
 	await interaction.reply({
-		embeds: [
+		flags: MessageFlags.IsComponentsV2,
+
+		components: [
 			{
-				title: "Scratch Embeds Settings",
-				color: constants.themeColor,
-				description: `**Enabled**: ${constants.emojis.statuses[config.enabled ? "yes" : "no"]}\n\n${
-					config.channels.size ? `**Channel Overrides**:\n` : "No channel overrides"
-				}${Array.from(
-					config.channels.entries(),
-					([channel, status]) =>
-						`${channelMention(channel)}: ${constants.emojis.statuses[status ? "yes" : "no"]}`,
-				).join("\n")}`,
+				type: ComponentType.Container,
+				accentColor: constants.themeColor,
+				components: [
+					{ type: ComponentType.TextDisplay, content: "## Scratch Embeds Settings" },
+					{
+						type: ComponentType.TextDisplay,
+						content: `**Enabled**: ${constants.emojis.statuses[config.enabled ? "yes" : "no"]}`,
+					},
+					{ type: ComponentType.TextDisplay, content: `## Channel Overrides` },
+					{
+						type: ComponentType.TextDisplay,
+						content:
+							Array.from(
+								config.channels.entries(),
+								([channel, status]) =>
+									`${channelMention(channel)}: ${constants.emojis.statuses[status ? "yes" : "no"]}`,
+							).join("\n") || "No channel overrides",
+					},
+				],
 			},
 		],
 	});
